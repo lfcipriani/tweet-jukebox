03784c34b6e96610504e565060835ab3cc4f23e1
Luis Cipriani <lfcipriani@gmail.com>
2014-11-14 13:26:59 -0300

    config sample file

---
+++ config/config.sample.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // namespace: use to separate different context executions
+    namespace: "EDIT",
+    twitter: {
+        // add API keys here, see apps.twitter.com
+        api_keys: {
+            consumer_key: "EDIT",
+            consumer_secret: "EDIT",
+            access_token: "EDIT",
+            access_token_secret: "EDIT"
+        },
+        // jukebox: the user that will accept music requests
+        jukebox: "EDIT",
+        // admins: allowed users to send DM commands to control player
+        admins: ["EDIT"],
+        // captureStrategy: only streaming supported, will support rest in the future
+        capture_strategy: "streaming"
+    }
+}
+
