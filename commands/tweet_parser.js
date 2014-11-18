aac2ce7ed73112f8e35c7836f68fbf75ce77d8b0
Luis Cipriani <lfcipriani@gmail.com>
2014-11-18 00:17:11 -0200

    tweet parser: token, link and search commands working

--- commands/tweet_parser.js
+++ commands/tweet_parser.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var config = require('../config');
+var _ = require('underscore');
 
 var TweetRequest = function(type, via, fromUser, tweetId, param) {
     this.type = type;
@@ -11,31 +12,97 @@ var TweetRequest = function(type, via, fromUser, tweetId, param) {
 }
 
 var parseUtil = {
+
     extractToken: function(tweet) {
         var re = new RegExp("^@"+ config.twitter.jukebox +" (\\d{4})$", "i"); 
         var result = tweet.text.match(re);
         if (result) { result = result[1] }
         return result;
     },
-    extractHashCommand: function(tweet) {},
-    extractUrl: function(tweet) {},
-    extractSearch: function(tweet) {}
+
+    extractUrl: function(tweet) {
+        if (tweet["entities"] && tweet["entities"]["urls"]) {
+            var urlList = tweet["entities"]["urls"];
+            for (var u = 0; u < urlList.length; u++) {
+                // SPOTIFY
+                //https://embed.spotify.com/?uri=spotify:track:4v0tapCyBcdyEbIpd1zZGU"
+                //spotify:track:4v0tapCyBcdyEbIpd1zZGU
+                //http://open.spotify.com/track/4v0tapCyBcdyEbIpd1zZGU
+                //https://play.spotify.com/track/4v0tapCyBcdyEbIpd1zZGU?play=true&utm_source=open.spotify.com&utm_medium=open
+                var re = /spotify\.com\/track\/([^\&\?\/]+)|spotify:track:([^\&\?\/]+)/
+                var result = urlList[u].expanded_url.match(re);
+                result = _.reject(result, function(v) {return v == undefined;})
+                if (result.length > 1) {
+                    return {"spotify": result[1]};
+                }
+
+                // YOUTUBE
+                // youtube.com/watch?v=, youtube.com/embed/, youtube.com/v/, youtu.be/
+                var re = /youtube\.com\/watch\?v=([^\&\?\/]+)|youtube\.com\/embed\/([^\&\?\/]+)|youtube\.com\/v\/([^\&\?\/]+)|youtu\.be\/([^\&\?\/]+)/;
+                var result = urlList[u].expanded_url.match(re);
+                result = _.reject(result, function(v) {return v == undefined;})
+                if (result.length > 1) {
+                    return {"youtube": result[1]};
+                }
+
+                // SOUNDCLOUD
+                // https://soundcloud.com/johnherrman/can-i-unfollow-please-at-zumba
+                var re = /soundcloud\.com\/([^\&\?\/]+)\/([^\&\?\/]+)/
+                var result = urlList[u].expanded_url.match(re);
+                result = _.reject(result, function(v) {return v == undefined;})
+                if (result.length > 1) {
+                    return {"soundcloud": result[1] + "/" + result[2]};
+                }
+            }
+        }
+        return null;
+    },
+
+    extractSearch: function(tweet) {
+        var text = tweet.text;
+        text = text.replace("@"+config.twitter.jukebox, ""); // cleaning reply mention
+
+        var terms = text.split(/\bby\b/);
+        var param = {};
+        var any = terms[0].match(/^\s(?:play\s)?(.*)/);
+        if (any && any[1] != "") {
+            param["any"] = [any[1]]; 
+        } 
+        if (terms.length > 1) {
+            param["artist"] = terms[1];
+        }
+
+        return (_.isEmpty(param) ? null : param);
+    }
 };
 
 module.exports = {
     parse: function(tweet) {
         var type = null;
+        var param = null;
 
         // checking if this is a token user verification 
-        var token = parseUtil.extractToken(tweet);
-        if (token) {
-            var type = "TOKEN";
-            var param = token;
+        param = parseUtil.extractToken(tweet);
+        if (param) {
+            type = "TOKEN";
         }
 
         // checking if user want to play an URL
         if (!type) {
-            return null;
+            param = parseUtil.extractUrl(tweet);
+            if (param) {
+                type = "LINK";
+            }
+        }
+
+        // checking if user want to search and play
+        if (!type) {
+            param = parseUtil.extractSearch(tweet);
+            if (param) {
+                type = "SEARCH";
+            } else {
+                return null;
+            }
         }
 
         var via = "tweet";
--- test/tweet_parser_test.js
+++ test/tweet_parser_test.js
@@ -20,6 +20,119 @@ describe('commands/tweet_parser', function(){
         assert.equal(result.param, "1234");
     });
 
+    it('should parse links from youtube', function() {
+        tweet = {
+            text: "@" + config.twitter.jukebox + " play ",
+            user: { screen_name: "lfcipriani"},
+            id_str: "1234567",
+            "entities": {
+                "urls": [
+                    {
+                        "indices": [76,97],
+                        "display_url": "dev.twitter.com/terms/display-…",
+                        "expanded_url": "https://www.youtube.com/watch?v=wZZ7oFKsKzY",
+                        "url": "https://t.co/Ed4omjYs"
+                    }
+                ]
+            }
+        };
+
+        var result = tweetParser.parse(tweet);
+
+        assert.equal(result.type, "LINK");
+        assert.equal(result.param["youtube"], "wZZ7oFKsKzY");
+    });
+
+    it('should parse links from spotify', function() {
+        tweet = {
+            text: "@" + config.twitter.jukebox + " play ",
+            user: { screen_name: "lfcipriani"},
+            id_str: "1234567",
+            "entities": {
+                "urls": [
+                    {
+                        "indices": [76,97],
+                        "display_url": "dev.twitter.com/terms/display-…",
+                        "expanded_url": "http://open.spotify.com/track/4v0tapCyBcdyEbIpd1zZGU",
+                        "url": "https://t.co/Ed4omjYs"
+                    }
+                ]
+            }
+        };
+
+        var result = tweetParser.parse(tweet);
+
+        assert.equal(result.type, "LINK");
+        assert.equal(result.param["spotify"], "4v0tapCyBcdyEbIpd1zZGU");
+    });
+
+    it('should parse links from soundcloud', function() {
+        tweet = {
+            text: "@" + config.twitter.jukebox + " play madonna",
+            user: { screen_name: "lfcipriani"},
+            id_str: "1234567",
+            "entities": {
+                "urls": [
+                    {
+                        "indices": [76,97],
+                        "display_url": "dev.twitter.com/terms/display-…",
+                        "expanded_url": "https://soundcloud.com/lfcipriani/beastie-boys-i-dontt-know",
+                        "url": "https://t.co/Ed4omjYs"
+                    }
+                ]
+            }
+        };
+
+        var result = tweetParser.parse(tweet);
+
+        assert.equal(result.type, "LINK");
+        assert.equal(result.param["soundcloud"], "lfcipriani/beastie-boys-i-dontt-know");
+    });
+
+    it('should parse search requests for music', function() {
+        tweet = {
+            text: "@" + config.twitter.jukebox + " play like a virgin by madonna",
+            user: { screen_name: "lfcipriani"},
+            id_str: "1234567"
+        };
+        var result = tweetParser.parse(tweet);
+
+        assert.equal(result.type, "SEARCH");
+        assert.notEqual(result.param["any"][0].indexOf("like a virgin"), -1);
+        assert.notEqual(result.param["artist"].indexOf("madonna"), -1);
+
+        tweet = {
+            text: "@" + config.twitter.jukebox + " play like a virgin",
+            user: { screen_name: "lfcipriani"},
+            id_str: "1234567"
+        };
+        var result = tweetParser.parse(tweet);
+
+        assert.equal(result.type, "SEARCH");
+        assert.notEqual(result.param["any"][0].indexOf("like a virgin"), -1);
+        assert.equal(result.param["artist"], undefined);
+
+        tweet = {
+            text: "@" + config.twitter.jukebox + " play by madonna",
+            user: { screen_name: "lfcipriani"},
+            id_str: "1234567"
+        };
+        var result = tweetParser.parse(tweet);
+
+        assert.equal(result.type, "SEARCH");
+        assert.equal(result.param["any"], undefined);
+        assert.notEqual(result.param["artist"].indexOf("madonna"), -1);
+
+        tweet = {
+            text: "@" + config.twitter.jukebox + "",
+            user: { screen_name: "lfcipriani"},
+            id_str: "1234567"
+        };
+        var result = tweetParser.parse(tweet);
+
+        assert.equal(result, null);
+    });
+
   });
 
 });
