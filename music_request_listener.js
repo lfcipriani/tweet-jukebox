05c7996e86f3194b7dfb61e4e9c828061b4ff6a7
Luis Cipriani <lfcipriani@gmail.com>
2014-11-20 21:37:53 -0200

    getting requests from twitter

--- music_request_listener.js
+++ music_request_listener.js
@@ -2,9 +2,35 @@
 
 var config = require('./config');
 var Stream = require('./twitter/' + config.twitter.capture_strategy + '_strategy');
+var TweetParser = require('./commands/tweet_parser');
+var Music = require('./music_controller/mopidy');
 
 Stream.init();
 
+var currentTrack = 0;
+
 Stream.onTweet(function(tweet) {
-   console.log(tweet.text); 
+    var request = TweetParser.parse(tweet);
+
+    console.log("Tweet: "+tweet.text);
+    if (request) {
+        console.log("Request: " + request.type + " " + JSON.stringify(request.param));
+        if (request.type == "SEARCH") {
+            var music = Music.search(request.param, function(m) {
+                if (m) {
+                    console.log("Music: " + m.name + " ("+m.uri+")" );
+                    Music.add(m.uri, function() {
+                        Music.getPlayerState(function(state){
+                            if (state != "playing") {
+                                Music.play();
+                            }
+                        });
+                    });
+                } else {
+                    console.log("Didn't find a music...");
+                }
+            });
+        }
+    }
 });
+
