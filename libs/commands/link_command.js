a8f894f0589daeeaee0eb26da81f04aa2638d257
Luis Cipriani <lfcipriani@gmail.com>
2014-11-26 00:26:18 -0200

    adding the ability to play links requested by users

---
+++ libs/commands/link_command.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var _ = require('underscore');
+var Twitter = require('../twitter_post');
+var Track = require('../track_urls');
+
+module.exports = function(musicController, cmd) {
+
+    var mopidy = musicController.getMopidyObj();
+
+    function success(request, data) {
+        if (cmd.post_reply_on_success) {
+            Twitter.reply("Thanks! Will be playing " + Track.getUrl(data) + " soon", request);
+        }
+    }
+
+    function error(request) {
+        if (cmd.post_reply_on_error) {
+            Twitter.reply("Sorry! Not able to find music.", request);
+        }
+    }
+
+    function addMusic(request, track) {
+        console.log("Music: " + track.name);
+        musicController.add(track.uri, function(data) {
+            musicController.getPlayerState(function(state){
+                if (state != "playing") {
+                    musicController.play();
+                }
+            });
+            success(request, track);
+        });
+    }
+
+    return {
+        run: function(request) {
+
+            if (request.param.source == "spotify") {
+                mopidy.library.lookup({"uri":"spotify:track:"+request.param.uri_part}).then(function(data) {
+                    if (data.length > 0) {
+                        addMusic(request, data[0]);
+                    } else {
+                        error(request);
+                    } 
+                });
+            } else {
+                mopidy.library.search({"query":{"any": [request.param.uri_part] },"uris":[request.param.source + ":"]}).then(function(data) {
+                    var resultTracks = [];
+
+                    _.each(["spotify", "youtube", "soundcloud"], function(source){
+                        _.each(data, function(result) {
+                            if (_.str.startsWith(result.uri, source)) {
+                                if (result.tracks) {
+                                    _.each(result.tracks, function(t) {
+                                        resultTracks.push(t);
+                                    });
+                                }
+                            }
+                        });
+                    });
+
+                    if (resultTracks.length > 0) {
+                        addMusic(request, resultTracks[0]);
+                    } else {
+                        error(request);
+                    } 
+                });
+            }
+
+        }
+    };
+};
